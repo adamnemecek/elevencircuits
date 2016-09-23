5c7981f490aa44dc45a1d83a6bd47e17863ad3db
jsbean <jbean46@gmail.com>
2016-09-23 14:08:20 -0400

    Inject F53OSC directly into project; refactor out later if possible

--- elevencircuits/ViewController.swift
+++ elevencircuits/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         let labyrinth = Labyrinth(amountCircuits: 11)
         
         let timeline = Timeline()
