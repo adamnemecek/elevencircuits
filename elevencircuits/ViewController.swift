dc3893810e28343fea171fdb41ebec2179694245
jsbean <jbean46@gmail.com>
2016-09-23 13:30:32 -0400

    Create circle animated for duration of Path segment section

--- elevencircuits/ViewController.swift
+++ elevencircuits/ViewController.swift
@@ -13,42 +13,64 @@ import Timeline
 
 class ViewController: UIViewController {
 
+    var circleLayer: CAShapeLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let w: CGFloat = 40
-        let center = view.frame.width / 2 - 0.5 * w
-        let square = CAShapeLayer()
-        square.path = UIBezierPath(rect: CGRect(x: center, y: 600, width: w, height: w)).cgPath
-        square.fillColor = UIColor.red.cgColor
-        view.layer.addSublayer(square)
-        
-        func on() {
-            CATransaction.setDisableActions(true)
-            square.fillColor = UIColor.white.cgColor
-            CATransaction.setDisableActions(false)
-        }
-        
-        func off() {
-            CATransaction.setDisableActions(true)
-            square.fillColor = UIColor.black.cgColor
-            CATransaction.setDisableActions(false)
-        }
-        
         let labyrinth = Labyrinth(amountCircuits: 11)
         
         let timeline = Timeline()
         
-        labyrinth.path
-            .map { $0.length }
-            .cumulative
-            .map { $0 * 10 }
-            .forEach {
-                timeline.add(at: Double($0), action: on)
-                timeline.add(at: Double($0 + 0.2), action: off)
-            }
+        let lengths = labyrinth.path.map { $0.length } + labyrinth.path.reversed().map { $0.length }
+        
+        
+        print(lengths.map { $0 * 10 }.sum / 60)
+            
+        let cumulativeLengths = zip(
+            lengths.cumulative.map { $0 * 10 },
+            lengths.map { $0 * 10 }
+        )
+        
+        lengths.forEach { print($0) }
+            
+        cumulativeLengths.forEach { (offset, length) in
+            timeline.add(at: Double(offset)) { self.animateCircle(duration: Double(length)) }
+        }
         
         timeline.start()
+        
+        let startAngle = CGFloat((3 * M_PI) / 2)
+        let endAngle = startAngle + CGFloat(2 * M_PI)
+        
+        let circlePath = UIBezierPath(
+            arcCenter: CGPoint(x: view.frame.size.width / 2.0, y: view.frame.size.height / 2.0),
+            radius: (view.frame.size.width - 10) / 2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true
+        )
+        
+        self.circleLayer = CAShapeLayer()
+        circleLayer.path = circlePath.cgPath
+        circleLayer.fillColor = nil
+        circleLayer.strokeColor = UIColor.red.cgColor
+        circleLayer.lineWidth = 5
+        
+        // don't draw yet
+        circleLayer.strokeEnd = 0.0
+        
+        view.layer.addSublayer(circleLayer)
+    }
+    
+    // TODO: animateCircle(radius:duration:)
+    func animateCircle(duration: Double) {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = duration
+        animation.fromValue = 0
+        animation.toValue = 1
+        circleLayer.strokeEnd = 1.0
+        circleLayer.add(animation, forKey: "animateCircle")
     }
 
     override func didReceiveMemoryWarning() {
@@ -56,4 +78,3 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
-
