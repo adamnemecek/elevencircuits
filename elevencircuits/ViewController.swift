2cf52201b4d42ef3a5f8939895c845c2e5048041
jsbean <jbean46@gmail.com>
2016-10-17 12:44:53 -0400

    Add start/stop control, add structural events

--- elevencircuits/ViewController.swift
+++ elevencircuits/ViewController.swift
@@ -25,47 +25,227 @@ class ViewController: UIViewController, F53OSCPacketDestination {
     var rightWidth: CGFloat { return view.frame.width - leftWidth }
     var rightCenter: CGFloat { return view.frame.width - (0.5 * rightWidth) }
     
+    var startButton: UIButton!
+    var stopButton: UIButton!
+    
+    let lengths: [Double] = [
+        
+        15.9436, // 0
+        14.2353,
+        11.3883,
+        
+        14.8048, // 3
+        9.11062,
+        10.8189,
+        25.0542,
+        
+        10.8189, // 7
+        18.2212,
+        7.40238,
+        11.3883,
+        
+        7.40238, // 11
+        9.11062,
+        21.6377,
+        12.5271,
+        28.4707,
+        15.9436,
+        35.3036,
+        19.3601,
+        21.0683,
+        45.5531,
+        
+        21.0683, // 21
+        38.7201,
+        17.6518,
+        31.8872,
+        
+        17.6518, // 25
+        19.3601,
+        42.1366,
+        45.5531,
+        
+        14.2353, // 29
+        12.5271
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        createStartButton()
+        createStopButton()
         createDivider()
         createCircleProgressBar()
         createGraphicEqualizer()
         createNanoKontrol()
+        setInitialValues()
+        constructTimeline()
+    }
+    
+    @objc private func start() {
+        setInitialValues()
+        timeline.start()
+        startButton.removeFromSuperview()
+        view.addSubview(stopButton)
+    }
+    
+    @objc private func stop() {
+        createNanoKontrol()
+        createGraphicEqualizer()
+        setInitialValues()
+        timeline.stop()
+        circleLayer.removeFromSuperlayer()
+        stopButton.removeFromSuperview()
+        view.addSubview(startButton)
+    }
+    
+    private func createStartButton() {
+        let width: CGFloat = 300
+        let left = leftCenter - 0.5 * width
+        let height: CGFloat = 150
+        let top = 0.5 * view.frame.height - 0.5 * height
+        startButton = UIButton(frame: CGRect(x: left, y: top, width: width, height: height))
+        startButton.setTitle("Start", for: .normal)
+        startButton.setTitleColor(.white, for: .normal)
+        startButton.titleLabel?.font = UIFont(name: "Avenir-Next", size: 18)
+        startButton.layer.opacity = 0.5
+        startButton.layer.borderWidth = 1
+        startButton.layer.borderColor = UIColor.white.cgColor
         
-        let labyrinth = Labyrinth(amountCircuits: 11)
-        let lengths = labyrinth.path.map { $0.length } + labyrinth.path.reversed().map { $0.length }
-        print(lengths.map { $0 * 10 }.sum / 60)
-            
-        let cumulativeLengths = zip(
-            lengths.cumulative.map { $0 * 10 },
-            lengths.map { $0 * 10 }
-        )
+        startButton.addTarget(self, action: #selector(start), for: .touchDown)
+
+        view.addSubview(startButton)
+    }
+    
+    private func createStopButton() {
+        let width: CGFloat = 300
+        let left = leftCenter - 0.5 * width
+        let height: CGFloat = 150
+        let top = 0.5 * view.frame.height - 0.5 * height
+        stopButton = UIButton(frame: CGRect(x: left, y: top, width: width, height: height))
+        stopButton.setTitle("Stop", for: .normal)
+        stopButton.setTitleColor(.white, for: .normal)
+        stopButton.titleLabel?.font = UIFont(name: "Avenir-Next", size: 18)
+        stopButton.layer.opacity = 0.5
+        stopButton.layer.borderWidth = 1
+        stopButton.layer.borderColor = UIColor.white.cgColor
         
+        stopButton.addTarget(self, action: #selector(stop), for: .touchDown)
+    }
+    
+    private func createCueLabel() {
+        
+    }
+    
+    // TODO: 
+    private func createSectionLabel() {
+        
+    }
+    
+    private func setInitialValues() {
+        setGraphicEqualizerInitialValues()
+        setNanoKontrolInitialValues()
+    }
+    
+    private func setGraphicEqualizerInitialValues() {
         graphicEqualizer["GAIN"].ramp(to: 1)
-        graphicEqualizer[10].ramp(to: 0.5)
+        graphicEqualizer["31.25"].ramp(to: 0.5)
+        graphicEqualizer["16K"].ramp(to: 0.5)
+    }
+    
+    private func setNanoKontrolInitialValues() {
         
-        nanoKontrol.channels[1].dial.ramp(to: 1)
-        nanoKontrol.channels[3].slider.ramp(to: 0.33, over: 3)
-        nanoKontrol.channels[3].dial.ramp(to: 0.6, over: 10)
+        // Set bass to thoughtful level, to be increased in section 4
+        nanoKontrol.channels[0].slider.ramp(to: 0.5)
         
-        lengths.forEach { print($0) }
-            
-        cumulativeLengths.forEach { (offset, length) in
-            timeline.add(at: Double(offset)) {
-                
-                let slider = UInt(Int.random(min: 0, max: 11))
-                let eqValue = Float.random()
-                
-                self.graphicEqualizer[slider].ramp(to: eqValue, over: Double(length))
-                self.animateCircle(duration: Double(length))
+        // Set sin/saw to barely present level
+        nanoKontrol.channels[3].slider.ramp(to: 0.25)
+        
+        // Set sin/saw to saw
+        nanoKontrol.channels[3].dial.ramp(to: 1)
+    }
+    
+    private func constructTimeline() {
+        addCircleProgressBarEvents()
+        addGraphicEqualizerEvents()
+        addNanoKontrolEvents()
+        print("timeline: \(timeline)")
+    }
+    
+    private func addCircleProgressBarEvents() {
+        for (length, cumulative) in zip(lengths, lengths.cumulative) {
+            timeline.add(at: cumulative) {
+                self.animateCircle(duration: length)
             }
         }
+    }
+    
+    // EQ EVENTS
+    
+    // TODO: Refine values, though this is a solid structure
+    private func addGraphicEqualizerEvents() {
+        rampEQ("31.25", to: 0.25, for: 0)
+        rampEQ("16K", to: 0.25, for: 2)
+        rampEQ("31.25", to: 0.5, for: 4)
+        rampEQ("16K", to: 0.5, for: 6)
+        rampEQ("31.25", to: 0.25, for: 8)
+        rampEQ("16K", to: 0.25, for: 10)
+        rampEQ("31.25", to: 0.5, for: 12)
+        rampEQ("16K", to: 0.5, for: 14)
+        rampEQ("31.25", to: 0.25, for: 16)
+        rampEQ("16K", to: 0.25, for: 18)
+        rampEQ("31.25", to: 0.5, for: 20)
+        rampEQ("16K", to: 0.5, for: 22)
+        rampEQ("31.25", to: 0.25, for: 24)
+        rampEQ("16K", to: 0.25, for: 26)
+        rampEQ("31.25", to: 0.5, for: 28)
+        rampEQ("16K", to: 0.5, for: 30)
+    }
+    
+    private func rampEQ(_ band: String, to value: Float, for event: Int) {
+        timeline.add(at: lengths.cumulative[event]) {
+            self.graphicEqualizer[band].ramp(to: value, over: self.lengths[event])
+        }
+    }
+   
+    private func add(for index: Int, action: @escaping () -> ()) {
+        timeline.add(at: lengths.cumulative[index]) {
+            action()
+        }
+    }
+    
+    // NANOKONTROL EVENTS
+    
+    private func addNanoKontrolEvents() {
         
-        timeline.start()
+        rampNanoKontrolSlider(3, to: 1, over: 11..<21)
+        
+        // Crossfade saw -> sin
+        rampNanoKontrolDial(3, to: 1, at: lengths.sum, over: 30)
+    }
+    
+    private func rampNanoKontrolDial(_ channel: Int, to value: Float, at offset: Double, over duration: Double) {
+        timeline.add(at: offset) {
+            self.nanoKontrol.channels[channel].dial.ramp(to: value, over: duration)
+        }
+    }
+    
+    private func rampNanoKontrolSlider(_ channel: Int, to value: Float, for event: Int) {
+        timeline.add(at: lengths.cumulative[event]) {
+            self.nanoKontrol.channels[channel].slider.ramp(to: value, over: self.lengths[event])
+        }
+    }
+    
+    private func rampNanoKontrolSlider(_ channel: Int, to value: Float, over events: Range<Int>) {
+        timeline.add(at: lengths.cumulative[events.lowerBound]) {
+            self.nanoKontrol.channels[channel].slider.ramp(
+                to: value,
+                over: self.lengths[events].sum
+            )
+        }
     }
     
     private func createNanoKontrol() {
+        nanoKontrol?.removeFromSuperlayer()
         self.nanoKontrol = NanoKontrolView(
             frame: CGRect(x: 700, y: 100, width: 400, height: 300)
         )
@@ -85,6 +265,7 @@ class ViewController: UIViewController, F53OSCPacketDestination {
     }
     
     private func createGraphicEqualizer() {
+        graphicEqualizer?.removeFromSuperlayer()
         let width = rightWidth - 200
         let height = 1/2 * width
         self.graphicEqualizer = GraphicEqualizerView(
